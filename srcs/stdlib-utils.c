f2944d6acffc3f76f349440109b7798138d1216c
Chihab <chihab.moussadyk@gmail.com>
2025-02-20 18:05:14 +0100

    Small funny correction

--- srcs/stdlib-utils.c
+++ srcs/stdlib-utils.c
@@ -4,9 +4,15 @@ unsigned int ft_strlen(char *str)
 {
 	unsigned int i;
 
-	i = 0;
+#if 0
+	/*
+	 * NOTE(chichi): 
+	 * 	Cringest thing you could have presented me
+	 */
 	if (!str)
 		return (0);
+#endif
+	i = 0;
 	while (str[i])
 		i++;
 	return (i);
